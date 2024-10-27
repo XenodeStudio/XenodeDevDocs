4f0c8c92fe1a63b6dbc6a3fce786327b80cc5728
Shreyas <69378056+shreyaspranav@users.noreply.github.com>
2024-10-27 13:24:17 +0530

    Added Metadata for Google SEO

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,5 +14,13 @@ export default {
         footer: {
             message: 'Built with love by the Xenode Studio team.'
         }
-    }
+    },
+
+    // Metadata for Google SEO:
+    head: [
+        ['meta', {
+            name: 'google-site-verification',
+            content: 'rk5w0mlpKvSpex5z5XJ4lhOn2nVFHLZPso7G8XzMh5M'
+        }]
+    ]
 }

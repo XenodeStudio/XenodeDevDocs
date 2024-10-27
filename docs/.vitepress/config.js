44e6c354bfaddf45339f4299f5c42ad2faa537dc
Shreyas <69378056+shreyaspranav@users.noreply.github.com>
2024-10-27 14:03:50 +0530

    Fixed the sitemap

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+
 export default {
     base: '/XenodeDevDocs/',
 
@@ -24,5 +27,35 @@ export default {
             name: 'google-site-verification',
             content: 'rk5w0mlpKvSpex5z5XJ4lhOn2nVFHLZPso7G8XzMh5M'
         }]
-    ]
+    ],
+
+    buildEnd() {
+        // Define the sitemap content
+        const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+          <url>
+            <loc>https://xenodestudio.github.io/XenodeDevDocs/</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+          </url>
+
+          <url>
+            <loc>https://xenodestudio.github.io/contributing</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+          </url>
+
+          <url>
+            <loc>https://xenodestudio.github.io/code_of_conduct</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+          </url>
+        </urlset>`;
+    
+        // Write the sitemap to the dist directory
+        fs.writeFileSync(path.resolve(__dirname, 'dist/sitemap.xml'), sitemapContent);
+    }
 }
